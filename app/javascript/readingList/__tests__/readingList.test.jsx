d0baadf1a382940c3d1048e6f03ebdd2b6506d1f
Harry Borrelli <hborrelli@outlook.com>
2020-05-11 09:54:48 -0600

    Stuck on testing. Not sure how to see what is currently being rendered.

--- app/javascript/readingList/__tests__/readingList.test.jsx
+++ app/javascript/readingList/__tests__/readingList.test.jsx
@@ -1,4 +1,5 @@
 import { h } from 'preact';
+import 'preact/debug';
 import render from 'preact-render-to-json';
 import { ReadingList } from '../readingList';
 
@@ -6,5 +7,11 @@ describe('<ReadingList />', () => {
   it('renders properly', () => {
     const tree = render(<ReadingList availableTags={['discuss']} />);
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should load articles', () => {
+    const { getByText } = render(<ReadingList availableTags={['discuss']} />);
+
+    expect(getByText('hi')).toBeInTheDocument();
   });
 });

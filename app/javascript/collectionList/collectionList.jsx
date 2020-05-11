402117b6dee7e0875bfc83681a0b57c2be5c9c34
Harry Borrelli <hborrelli@outlook.com>
2020-05-11 14:42:03 -0600

    Add form to CollectionsLIst component.

--- app/javascript/collectionList/collectionList.jsx
+++ app/javascript/collectionList/collectionList.jsx
@@ -1,5 +1,11 @@
 import { h } from 'preact';
+import CollectionForm from '../collectionForm/collectionForm';
 
 export const CollectionList = () => {
-  return <div>Yayyy, collections!!!</div>;
+  return (
+    <div>
+      Yayyy, collections!!!
+      <CollectionForm />
+    </div>
+  );
 };

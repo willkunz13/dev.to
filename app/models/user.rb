672c169ab6cec8b24123f4d1b59e381d6f0139f5
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-11 16:22:54 -0600

    Fix name in relationship for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,7 +61,7 @@ class User < ApplicationRecord
   has_many :created_podcasts, class_name: "Podcast", foreign_key: :creator_id, inverse_of: :creator, dependent: :nullify
 
   ## Mod4 contribution
-  has_many :parlaiments, dependent: :destroy
+  has_many :collection_lists, dependent: :destroy
 
   mount_uploader :profile_image, ProfileImageUploader
 

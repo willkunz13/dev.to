dad1cf772bba1cffd8178463ec6a2b3af42b310d
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-12 10:08:19 -0600

    Fix collection_list factory

--- spec/factories/collection_lists.rb
+++ spec/factories/collection_lists.rb
@@ -2,10 +2,4 @@ FactoryBot.define do
   factory :collection_list do
     user
   end
-
-  trait :with_articles do
-    after(:create) do |collection_list|
-      create_list(:article, 3, user: collection_list.user)
-    end
-  end
 end
